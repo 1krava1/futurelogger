e6fe4ae63d18fff5a2e97e6fc6fe4dcf34fcf5a6
Nick <krava9519@gmail.com>
2016-09-27 09:51:07 +0300

    clean db_config

--- inc/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-if( !defined('APP_VERSION') ) exit('Access denied.');
-	$db_config = array(
-				'db_host' => 'localhost',
-				'db_name' => 'mmm',
-				'db_user' => 'root',
-				'db_pass' => '',
-			);
-?>
